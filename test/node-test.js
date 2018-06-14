6904d0587ee353618b92db5ea2071e2879d17280
Michael Recachinas <mgr3yp@virginia.edu>
2018-06-14 21:49:05 +0100

    Add another CI test

--- test/node-test.js
+++ test/node-test.js
@@ -17,6 +17,7 @@ describe('sigfile.js', function() {
       var sigfile = require("../js/sigfile");
       var BlueHeader = sigfile.bluefile.BlueHeader;
       var file = fs.readFileSync("./test/dat/keyword_test_file.tmp");
+      console.log(file);
       console.log("BUFFER", file.buffer.slice(file.byteOffset, file.byteLength));
       var buf = file.buffer.slice(file.byteOffset, file.byteLength)
       console.log(ab2str(buf.slice(4, 8)));
