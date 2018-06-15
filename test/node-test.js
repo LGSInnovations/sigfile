90287d38091af9b111f8367d77bd768987b5d8c1
Michael Recachinas <mgr3yp@virginia.edu>
2018-06-15 23:07:48 +0100

    More testing

--- test/node-test.js
+++ test/node-test.js
@@ -16,40 +16,37 @@ describe('sigfile.js', function() {
       var fs = require("fs");
       var sigfile = require("../js/sigfile");
       var BlueHeader = sigfile.bluefile.BlueHeader;
-      var file = fs.readFileSync("./test/dat/keyword_test_file.tmp");
-      console.log(file);
-      console.log(file.buffer);
-      console.log(file.byteOffset, file.byteLength);
-      console.log("BUFFER", file.buffer.slice(file.byteOffset, file.byteLength));
-      var buf = file.buffer.slice(file.byteOffset, file.byteLength)
-      console.log(ab2str(buf.slice(4, 8)));
-      var hdr = new BlueHeader(buf, {});
-      assert.equal(hdr.type, 1000);
-      assert.equal(hdr.format, 'SB');
-      assert.equal(hdr.size, 0);
-      assert.equal(hdr.ext_start, 1);
-      assert.equal(hdr.ext_size, 224);
-      assert.equal(hdr.data_start, 512);
-      assert.equal(hdr.data_size, 0);
+      fs.readFile("./test/dat/keyword_test_file.tmp", function(err, data) {
+        if (err) throw err;
+        var buf = data.buffer.slice(data.byteOffset, data.byteLength)
+        var hdr = new BlueHeader(buf, {});
+        assert.equal(hdr.type, 1000);
+        assert.equal(hdr.format, 'SB');
+        assert.equal(hdr.size, 0);
+        assert.equal(hdr.ext_start, 1);
+        assert.equal(hdr.ext_size, 224);
+        assert.equal(hdr.data_start, 512);
+        assert.equal(hdr.data_size, 0);
 
-      var keywords = {
-          B_TEST: 123,
-          I_TEST: 1337,
-          L_TEST: 113355,
-          X_TEST: 987654321,
-          F_TEST: 0.12345000356435776,
-          D_TEST: 9.87654321,
-          O_TEST: 255,
-          STRING_TEST: "Hello World",
-          B_TEST2: 99,
-          STRING_TEST2: "Goodbye World"
-      };
-      for (var prop in keywords) {
-          assert.equal(
-              hdr.ext_header[prop],
-              keywords[prop]
-          );
-      }
+        var keywords = {
+            B_TEST: 123,
+            I_TEST: 1337,
+            L_TEST: 113355,
+            X_TEST: 987654321,
+            F_TEST: 0.12345000356435776,
+            D_TEST: 9.87654321,
+            O_TEST: 255,
+            STRING_TEST: "Hello World",
+            B_TEST2: 99,
+            STRING_TEST2: "Goodbye World"
+        };
+        for (var prop in keywords) {
+            assert.equal(
+                hdr.ext_header[prop],
+                keywords[prop]
+            );
+        }
+      });
     });
   });
 });
